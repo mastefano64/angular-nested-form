a25d5a4e6f4a6f5c347ab43ff81a292cb74f95fc
stefano marchisio <stamarch@virgilio.it>
2021-05-15 21:40:53 +0200

    commit3

--- src/app/residence/residence.component.ts
+++ src/app/residence/residence.component.ts
@@ -12,15 +12,5 @@ export class Residence2Component {
   form: NgForm;
 
   constructor() { }
-
-  onSubmit(form: NgForm) {
-    this.form = form.value
-    const firstname = form.value.firstname;
-    const lastname = form.value.lastname;
-    const address = form.value.residence.address;
-    const province = form.value.residence.province;
-    const city = form.value.residence.city;
-    const cap = form.value.residence.cap;    
-  }
   
 }
